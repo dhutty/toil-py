5e8d76a8cedb94e01294126f8f740a80a137f6b3
Andrew Charles Love <aclove@Andrews-MacBook-Pro.local>
2018-05-03 14:49:15 -0400

    buil 1.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.1',
 
     description='toil - Hard working python framework with boots and eyes on the ground for your cloud. Built in workers include OCI, AWS, CHEF, SCALR, REALTIONAL DATA SOURCES.',
     long_description=long_description,
